7462a856e7721cb4a5f975b40d94e16bd899090d
gestion-activos <gestion.activos2024@gmail.com>
2025-03-20 11:26:52 -0500

    Logos de brands agregados

--- src/components/Brands/brandsData.tsx
+++ src/components/Brands/brandsData.tsx
@@ -2,46 +2,25 @@ import { Brand } from "@/types/brand";
 
 const brandsData: Brand[] = [
   {
-    id: 6,
-    name: "Formbold",
-    href: "https://formbold.com",
-    image: "/images/brands/formbold.svg",
-    imageLight: "/images/brands/formbold-light.svg",
-  },
-  {
     id: 1,
-    name: "UIdeck",
-    href: "https://uideck.com",
-    image: "/images/brands/uideck.svg",
-    imageLight: "/images/brands/uideck-light.svg",
+    name: "Todas Florecemos",
+    href: "https://www.instagram.com/todasflorecemos/",
+    image: "/images/brands/todasflorecemos.svg",
+    imageLight: "/images/brands/todasflorecemos-light.svg",
   },
   {
     id: 2,
-    name: "Tailgrids",
-    href: "https://tailgrids.com",
-    image: "/images/brands/tailgrids.svg",
-    imageLight: "/images/brands/tailgrids-light.svg",
+    name: "Mujeres de Luz",
+    href: "https://www.instagram.com/fmujeresdeluz",
+    image: "/images/brands/mujeresdeluz.svg",
+    imageLight: "/images/brands/mujeresdeluz-light.svg",
   },
   {
     id: 3,
-    name: "Lineicons",
-    href: "https://lineicons.com",
-    image: "/images/brands/lineicons.svg",
-    imageLight: "/images/brands/lineicons-light.svg",
-  },
-  {
-    id: 4,
-    name: "Tailadmin",
-    href: "https://tailadmin.com",
-    image: "/images/brands/tailadmin.svg",
-    imageLight: "/images/brands/tailadmin-light.svg",
-  },
-  {
-    id: 5,
-    name: "PlainAdmin",
-    href: "https://plainadmin.com",
-    image: "/images/brands/plainadmin.svg",
-    imageLight: "/images/brands/plainadmin-light.svg",
+    name: "La Caja de Elena",
+    href: "https://www.instagram.com/lacajadeelena/",
+    image: "/images/brands/caja.svg",
+    imageLight: "/images/brands/caja-light.svg",
   },
 ];
 
--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,21 +1,19 @@
 import { Brand } from "@/types/brand";
 import Image from "next/image";
-
-const brandsData: Brand[] = []; // Lista vacía de marcas
+import brandsData from "@/components/Brands/brandsData"; // Asegúrate de que la ruta sea la correcta
 
 const Brands = () => {
   return (
     <section className="pt-16">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          
           <div className="w-full px-4">
             <div className="flex flex-wrap items-center justify-center rounded-sm bg-gray-light px-8 py-8 dark:bg-gray-dark sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]">
               {brandsData.length > 0 ? (
                 brandsData.map((brand) => <SingleBrand key={brand.id} brand={brand} />)
               ) : (
                 <p className="text-center text-gray-500 dark:text-gray-400">
-                  Aqui van entidades que confian en itwana.
+                  Aquí van entidades que confían en itwana.
                 </p>
               )}
             </div>
@@ -37,11 +35,25 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+        className="relative flex items-center justify-center h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
       >
-        <Image src={imageLight} alt={name} fill className="hidden dark:block" />
-        <Image src={image} alt={name} fill className="block dark:hidden" />
+        <Image
+          src={imageLight}
+          alt={name}
+          width={150}
+          height={50}
+          className="hidden dark:block"
+        />
+        <Image
+          src={image}
+          alt={name}
+          width={150}
+          height={50}
+          className="block dark:hidden"
+        />
       </a>
     </div>
   );
 };
+
+
